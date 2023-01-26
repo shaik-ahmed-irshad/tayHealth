b169e91afcc155a1b646db7dd1197ee55c220136
shaik-ahmed-irshad <edventurous.ahmed@gmail.com>
2023-01-26 23:51:06 +0530

    changes after live: v2

--- 1.js
+++ 1.js
@@ -59,3 +59,26 @@ if (auto) {
   // Run next slide at interval time
   slideInterval = setInterval(nextSlide, intervalTime);
 }
+
+
+
+        
+        
+$(document).ready(function() {
+
+  $('.counter').each(function () {
+$(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+}, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+      $(this).text(Math.ceil(now));
+  }
+});
+});
+
+});  
+
+
+
